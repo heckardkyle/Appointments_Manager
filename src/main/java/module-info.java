bc5b8109be2d82f0fe6ebc4dba35beb4eafe058c
Kyle Heckard <heckardkyle@yahoo.com>
2023-10-31 08:13:54 -0600

    Problem fixed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,6 @@ module com.c195_software_ii__advanced_java_concepts_pa {
     requires java.sql;
 
     opens com.c195_software_ii__advanced_java_concepts_pa to javafx.fxml;
-    opens com.c195_software_ii__advanced_java_concepts_pa.Controllers to javafx.fxml;
+    opens com.c195_software_ii__advanced_java_concepts_pa.Models to javafx.base;
     exports com.c195_software_ii__advanced_java_concepts_pa to javafx.fxml, javafx.graphics;
 }
