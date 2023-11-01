4a565b1e74b9aece2c05c053b74dd9d0952d7e35
Kyle Heckard <heckardkyle@yahoo.com>
2023-10-31 18:54:59 -0700

    LoginAlert.java:   Added if statements to check if an appointment lands within 15 minutes of login.   If so, builds alert statement.

--- src/main/java/com/c195_software_ii__advanced_java_concepts_pa/Utilities/LoginAlert.java
+++ src/main/java/com/c195_software_ii__advanced_java_concepts_pa/Utilities/LoginAlert.java
@@ -11,34 +11,55 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
 public class LoginAlert {
 
     public static void LoginAlert() throws SQLException {
 
+        // Get Local Date and Time
         LocalDate localDate = LocalDateTime.now(ZoneId.systemDefault()).toLocalDate();
         LocalTime localTime = LocalDateTime.now(ZoneId.systemDefault()).toLocalTime();
 
+        // Declare and get all appointments
         ObservableList<Appointment> allAppointments = FXCollections.observableArrayList();
         allAppointments = AppointmentDBImpl.getAllAppointments();
 
+        // Declare Appointment Variables
+        int appointmentIDAlert = 0;
+        LocalDate appointmentDateAlert = null;
+        LocalTime appointmentTimeAlert = null;
+        long timeDifference = 20;
+
+        // Declare alertString for Alert
         String alertString;
 
         for(Appointment appointment : allAppointments) {
+            LocalDate appointmentDate = appointment.getStartTime().toLocalDateTime().toLocalDate();
+            LocalTime appointmentTime = appointment.getStartTime().toLocalDateTime().toLocalTime();
 
+            if ((appointmentDate == localDate) && (ChronoUnit.MINUTES.between(appointmentTime, localTime) < timeDifference))
+                timeDifference = ChronoUnit.MICROS.between(appointmentTime, localTime);
+                appointmentIDAlert = appointment.getAppointmentID();
+                appointmentDateAlert = appointmentDate;
+                appointmentTimeAlert = appointmentTime;
+            }
+        if (timeDifference <= 15) { // Appointment within 15 minutes
+            alertString = "Upcoming appointment in " + timeDifference + " minutes\n"
+                    + "Appointment ID: " + appointmentIDAlert + "\n"
+                    + "Date: " + appointmentDateAlert + "\n"
+                    + "Time: " + appointmentTimeAlert;
         }
-
-        if ( 1 + 1 == 3) {
-            alertString = "";
-        }
-        else {
+        else { // no upcoming appointment
             alertString = "There are no upcoming appointments";
         }
 
+        //Issue alert Prompt
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setContentText(alertString);
         alert.showAndWait();
 
+        // Prevents Table list duplication bug
         allAppointments.removeAll();
     }
 
