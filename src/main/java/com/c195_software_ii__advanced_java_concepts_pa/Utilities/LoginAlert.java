d214ca34ce3d9167a7ff3e4cad66694e9b343cb3
Kyle Heckard <heckardkyle@yahoo.com>
2024-04-04 08:31:02 -0600

    LoginAlert.java   Completely overhauled and tested.

--- src/main/java/com/c195_software_ii__advanced_java_concepts_pa/Utilities/LoginAlert.java
+++ src/main/java/com/c195_software_ii__advanced_java_concepts_pa/Utilities/LoginAlert.java
@@ -1,60 +1,67 @@
 package com.c195_software_ii__advanced_java_concepts_pa.Utilities;
 
+import com.c195_software_ii__advanced_java_concepts_pa.AppointmentScheduler;
 import com.c195_software_ii__advanced_java_concepts_pa.DAO.AppointmentDBImpl;
 import com.c195_software_ii__advanced_java_concepts_pa.Models.Appointment;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Alert;
 
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 public class LoginAlert {
 
-    //
-    // Still needs to be tested
-    //
     public static void LoginAlert() throws SQLException {
 
-        // Get Local Date and Local Time
-        LocalDate localDate = LocalDateTime.now(ZoneId.systemDefault()).toLocalDate();
-        LocalTime localTime = LocalDateTime.now(ZoneId.systemDefault()).toLocalTime();
+        // Holds appointments that meet criteria
+        ObservableList<Appointment> upcomingAppointments = FXCollections.observableArrayList();
 
-        // Declare and get all appointments
-        ObservableList<Appointment> allAppointments = AppointmentDBImpl.getAllAppointments();
-
-        // Declare Appointment Variables for finding soonest appointment
-        long      timeDifference       = 20;
-        int       appointmentIDAlert   = 0;
-        LocalDate appointmentDateAlert = null;
-        LocalTime appointmentTimeAlert = null;
+        // Format to display appointment start time in
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm a v");
 
         // Declare alertString for Alert
         String alertString;
 
         // For each appointment
-        for(Appointment appointment : allAppointments) {
-            LocalDate appointmentDate = appointment.getDateTimeStart().toLocalDateTime().toLocalDate();
-            LocalTime appointmentTime = appointment.getDateTimeStart().toLocalDateTime().toLocalTime();
+        for(Appointment appointment : AppointmentDBImpl.getAllAppointments()) {
 
-            // Track nearest appointment in loop
-            if ((appointmentDate == localDate) && (ChronoUnit.MINUTES.between(appointmentTime, localTime) < timeDifference))
-                timeDifference       = ChronoUnit.MINUTES.between(appointmentTime, localTime);
-                appointmentIDAlert   = appointment.getAppointmentID();
-                appointmentDateAlert = appointmentDate;
-                appointmentTimeAlert = appointmentTime;
+            // if appointment start time is within 15 minutes of now, add to upcoming appointments list
+            if ((ChronoUnit.MINUTES.between(ZonedDateTime.now(ZoneId.systemDefault()), appointment.getDateTimeStart()) >= 0)
+                    && (ChronoUnit.MINUTES.between(ZonedDateTime.now(ZoneId.systemDefault()), appointment.getDateTimeStart()) <= 15)) {
+                upcomingAppointments.add(appointment);
             }
-        if (timeDifference <= 15) { // If appointment within 15 min, build alert
-            alertString = "Upcoming appointment in " + timeDifference + " minutes\n"
-                    + "Appointment ID: " + appointmentIDAlert + "\n"
-                    + "Date: " + appointmentDateAlert + "\n"
-                    + "Time: " + appointmentTimeAlert;
         }
-        else { // no upcoming appointment
+
+        // No upcoming appointments
+        if (upcomingAppointments.isEmpty()) {
             alertString = "There are no upcoming appointments";
+        }
+
+        // If only 1 upcoming appointment
+        else if (upcomingAppointments.size() == 1) {
+            alertString = "There is 1 upcoming appointment:\n"
+                    + "Appointment ID: " + upcomingAppointments.get(0).getAppointmentID() + "\n"
+                    + "Start time:     " + upcomingAppointments.get(0).getDateTimeStart().withZoneSameInstant(ZoneId.systemDefault()).format(format);
+        }
+
+        // If many upcoming appointments
+        else {
+            // Start string
+            alertString = "There are " + upcomingAppointments.size() + " upcoming appointments:";
+
+            // track appointment number in list
+            int index = 0;
+
+            // Each appointment, display number in list, ID, and start time
+            for (Appointment appointment : upcomingAppointments) {
+                alertString += "\n\n\tAppointment " + (index + 1) + "\n"
+                        + "Appointment ID: " + appointment.getAppointmentID() + "\n"
+                        + "Start time:     " + appointment.getDateTimeStart().withZoneSameInstant(ZoneId.systemDefault()).format(format);
+                index += 1;
+            }
         }
 
         //Issue alert Prompt
@@ -62,8 +69,8 @@ public class LoginAlert {
         alert.setContentText(alertString);
         alert.showAndWait();
 
-        // Prevents Table list duplication bug
-        allAppointments.removeAll();
+        // Clear table...just in case
+        upcomingAppointments.clear();
     }
 
 }
